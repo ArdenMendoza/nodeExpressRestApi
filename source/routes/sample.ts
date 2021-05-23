60b5d0d3232b19fcbe2124aa4e8b4fbfc6f54eb0
Arden Cristopher Mendoza <am@dewise.com>
2021-05-23 17:36:26 +0800

    WIP

---
+++ source/routes/sample.ts
@@ -0,0 +1,8 @@
+import express, { Router } from 'express';
+import controller from '../controllers/sample';
+
+const router = express();
+
+router.get('/ping', controller.sampleHealthCheck);
+
+export = router;
